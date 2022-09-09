409383730adffe8f91ff0ac9be64b9c63d1d6b7f
Alex <alex.plutta@googlemail.com>
2022-09-09 15:28:34 +0200

    fix tasks

--- deploy/01_Deploy_HelloGashawk.ts
+++ deploy/01_Deploy_HelloGashawk.ts
@@ -1,33 +1,47 @@
 import { Gashawk } from "@corpus-ventures/gashawk-sdk";
-import { ethers, Signer, Wallet } from "ethers";
+import { BigNumber, ethers, Wallet } from "ethers";
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
-import { HelloGashawk, HelloGashawk__factory } from "../typechain";
+import { HelloGashawk__factory } from "../typechain";
 
 const PROVIDER_URL = process.env.PROVIDER_URL || "";
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "Your private key";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    //For this example we just go with an random wallet
+    const PRIVATE_KEY = Wallet.createRandom().privateKey;
+    //Use you privateKey and a create an ethers wallet object
     const signer = new Wallet(PRIVATE_KEY, ethers.getDefaultProvider());
     //Creating a new instace of the Gashawk SDK using the signer to log in
     const gashawk = await Gashawk.fromSigner(signer, PROVIDER_URL);
-    const gashawkSigner = gashawk.getSinger();
-
+    //Use the gashawkSigner to sign transactions and send them to gashawk.
+    const gashawkSigner = gashawk.getSigner();
+    //ethers ContractFactory can be used to compile the smart contract code and create the deploy transaction
     const helloGashawkFactory = (await hre.ethers.getContractFactory(
-        "HelloGashawk",
-        gashawkSigner
+        "HelloGashawk"
     )) as HelloGashawk__factory;
 
-    const tx = await gashawkSigner.sendTransaction({
+  /*   //Use the gashawk signer to send the deploy transaction to gashawk
+    const contractDeployTx = await gashawkSigner.sendTransaction({
         ...helloGashawkFactory.getDeployTransaction(),
+        gasLimit: BigNumber.from(150000),
         customData: {
             //Instead of submitting your transaction gashawk can just simulate it for testing purposes
             simulate: true,
         },
-    });
+    }); */
 
-    await gashawk.wait(tx);
+    await gashawkSigner.sendTransaction({
+        to: "0x09c3d8547020a044c4879cD0546D448D362124Ae",
+        value: ethers.utils.parseEther("1.23"),
+        gasLimit: BigNumber.from(21000),
+        customData: {
+            simulate: true,
+        },
+    });
+    //If you want to watch the status of your transaction you can use the wait method. Another option is to visit beta.gashawk.io
+    //await gashawk.wait(contractDeployTx);
 };
 export default func;
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,16 +10,8 @@ import "dotenv/config";
 import "hardhat-deploy";
 import "solidity-coverage";
 
-import "./tasks/accounts";
-import "./tasks/balance";
-import "./tasks/block-number";
-import "./tasks/create-collectibles";
 require("hardhat-gas-reporter");
 import "hardhat-gas-reporter";
-
-// optional
-const PRIVATE_KEY =
-    "86c2bdee370cba0a7e185eb69d75e4546aa5872808409750d09f2c43f881a73c";
 
 module.exports = {
     defaultNetwork: "mainnet",
@@ -27,13 +19,6 @@ module.exports = {
     networks: {
         mainnet: {
             url: "https://eth-mainnet.alchemyapi.io/v2/fORbbzWLRjURSB-DaH1nx0ovkjLn2maI",
-            accounts: [PRIVATE_KEY],
-        },
-    },
-    namedAccounts: {
-        deployer: {
-            default: 0, // here this will by default take the first account as deployer
-            1: 0, // similarly on mainnet it will take the first account as deployer.
         },
     },
     solidity: {
